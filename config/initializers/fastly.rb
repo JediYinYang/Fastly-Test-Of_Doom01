b63c8b29dc593d9a665eb0a70f2aa5dd9481560a
JediYinYang <enderwigen19@hotmail.com>
2016-12-02 19:07:12 -0800

    Add files via upload

--- config/initializers/fastly.rb
+++ config/initializers/fastly.rb
@@ -1,6 +1,6 @@
 FastlyRails.configure do |c|
-  c.api_key = ENV[0ea1804316c9bdfb305b04adac8c0001] # Fastly api key, required
+  c.api_key = ENV['FASTLY_API_KEY'] # Fastly api key, required
   c.max_age = 86400 # time in seconds, optional, currently set to 1 day
-  c.service_id = ENV[5RqkiTHtoiwKXujXVnvQ3E] # The Fastly service you will be using, required
+  c.service_id = ENV['FASTLY_SERVICE_ID'] # The Fastly service you will be using, required
   c.purging_enabled = false unless ENV['ENABLE_PURGE']
 end
